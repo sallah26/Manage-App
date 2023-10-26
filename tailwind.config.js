74cb7b2f7c3663696f19698c34a3eb74d4505b40
sallah26 <143meski@gmail.com>
2023-10-26 21:56:55 +0300

    Starting the page and adjusting the hero page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,26 +3,30 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     screens: {
-      'xs': '450px',
-      // => @media (min-width: 450px) { ... }
-
-      'sm': '640px',
+      'sm': '480px',
       // => @media (min-width: 640px) { ... }
 
       'md': '768px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      'lg': '976px',
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      'xl': '1440px',
       // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
     },
-    extend: {},
+    extend: {
+      colors:{
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        VeryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 69%)',
+        veryLightGray: 'hsl(0, 100%, 98%)',
+      }
+    },
   },
   plugins: [],
 }
-
