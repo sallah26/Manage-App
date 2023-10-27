d52e2a7a753f020d3191f8d857b4e72a6f6a8161
sallah26 <143meski@gmail.com>
2023-10-27 09:19:12 +0300

    adding testimonials and footer pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
-        VeryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 69%)',
         veryLightGray: 'hsl(0, 100%, 98%)',
       }
